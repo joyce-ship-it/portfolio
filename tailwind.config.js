ad18ea123692006fa2d6938d4ab0f525675a43da
joycethomas <joycethomas2244@outlook.com>
2025-03-01 10:10:16 +0530

    create home page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lobster: ["Lobster", "cursive"], // Add your custom font here
+      },
+    },
+  },
+  plugins: [],
+};
